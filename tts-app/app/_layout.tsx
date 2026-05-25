1f04c27e54a2d10b555a07d1043144788f34abed
jwescottiv-pixel <jwescottiv@gmail.com>
2026-05-25 21:47:19 +0000

    Link RevenueCat to Supabase user

--- tts-app/app/_layout.tsx
+++ tts-app/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { ShareIntentProvider } from "expo-share-intent";
 import Purchases from "react-native-purchases";
 import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
 
 export default function RootLayout() {
 useEffect(() => {
@@ -9,7 +10,17 @@ useEffect(() => {
     apiKey: "test_YhbhHIgFcViNLNfpBcxAyADVxse",
   });
 
-  Purchases.logIn("test-user");
+supabase?.auth.getSession().then(({ data }) => {
+  const userId = data.session?.user?.id;
+
+  if (userId) {
+Purchases.logIn(userId);
+
+Purchases.getCustomerInfo().then((info) => {
+  console.log("REVENUECAT CUSTOMER:", info);
+});
+  }
+});
 }, []);
 return (
     <ShareIntentProvider>
